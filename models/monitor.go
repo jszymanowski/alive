e18d25e6bd4bb0db45e73c2b30e9a4eebde78b59
John Szymanowski <j.szymanowski@gmail.com>
2025-04-03 22:30:22 +0800

    Validate slug too

--- models/monitor.go
+++ models/monitor.go
@@ -10,7 +10,7 @@ type Monitor struct {
 	ID          uint   `gorm:"primaryKey"`
 	Name        string `gorm:"not null" validate:"required,min=3"`
 	Description string
-	Slug        string `gorm:"uniqueIndex"`
+	Slug        string `gorm:"uniqueIndex validate:"required,min=3"`
 	Status      string `gorm:"not null"` // TODO: add enum for status
 	UserID      uint   `gorm:"not null;foreignKey:ID;references:users"`
 
