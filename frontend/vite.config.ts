10bc212537d9897c2a33d48f267da626473c02c8
John Szymanowski <j.szymanowski@gmail.com>
2025-04-04 12:59:07 +0800

    Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type ViteUserConfig } from "vitest/config";
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),react(),
+    tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@root": path.resolve(__dirname, "./"),
+      "@tests": path.resolve(__dirname, "./tests"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.tsx",
+  },
+} as ViteUserConfig);
